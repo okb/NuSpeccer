22bcc4da81b7fab55c113f74e81229854baeb92c
bondehagen <anders@bondehagen.com>
2011-12-02 11:42:01 +0100

    Initial commit

---
+++ NuSpeccer/AssemblyInfoHelper.cs
@@ -0,0 +1,140 @@
+﻿#region License
+
+// --------------------------------------------------
+// Copyright © 2003–2010 OKB. All Rights Reserved.
+// 
+// This software is proprietary information of OKB.
+// USE IS SUBJECT TO LICENSE TERMS.
+// --------------------------------------------------
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace NuSpeccer
+{
+    public class AssemblyInfoHelper
+    {
+        private static Assembly mAssembly;
+
+
+        public AssemblyInfoHelper(Type type)
+        {
+            mAssembly = Assembly.GetAssembly(type);
+        }
+
+
+        public AssemblyInfoHelper(Assembly assembly)
+        {
+            mAssembly = assembly;
+        }
+
+
+        public string AssemblyName
+        {
+            get { return mAssembly.GetName().Name.ToString(); }
+        }
+
+        public string AssemblyVersion
+        {
+            get { return mAssembly.GetName().Version.ToString(); }
+        }
+
+        public string Company
+        {
+            get { return CustomAttributes<AssemblyCompanyAttribute>().Company; }
+        }
+
+        public string Copyright
+        {
+            get { return CustomAttributes<AssemblyCopyrightAttribute>().Copyright; }
+        }
+
+        public string Description
+        {
+            get { return CustomAttributes<AssemblyDescriptionAttribute>().Description; }
+        }
+
+        public string FileName
+        {
+            get
+            {
+                FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(mAssembly.Location);
+                return fvi.OriginalFilename;
+            }
+        }
+
+        public string FilePath
+        {
+            get
+            {
+                FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(mAssembly.Location);
+                return fvi.FileName;
+            }
+        }
+
+        public string FileVersion
+        {
+            get
+            {
+                FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(mAssembly.Location);
+                return fvi.FileVersion;
+            }
+        }
+
+        public string Guid
+        {
+            get { return CustomAttributes<GuidAttribute>().Value; }
+        }
+
+        public string Product
+        {
+            get { return CustomAttributes<AssemblyProductAttribute>().Product; }
+        }
+
+        public string RuntimeVersion
+        {
+            get { return mAssembly.ImageRuntimeVersion; }
+        }
+
+        public string Title
+        {
+            get { return CustomAttributes<AssemblyTitleAttribute>().Title; }
+        }
+
+        public string Trademark
+        {
+            get { return CustomAttributes<AssemblyTrademarkAttribute>().Trademark; }
+        }
+
+
+        public IDictionary<string, string> GetAllAttributes()
+        {
+            Dictionary<string, IList<CustomAttributeTypedArgument>> attributes = mAssembly.GetCustomAttributesData()
+                .ToDictionary(a => a.Constructor.ReflectedType.Name, a => a.ConstructorArguments);
+            return attributes
+                .Where(a => a.Key.StartsWith("Assembly"))
+                .ToDictionary(
+                    a => a.Key.Replace("Assembly", "").Replace("Attribute", ""),
+                    a => a.Value.First().Value.ToString()
+                );
+        }
+
+
+        private static T CustomAttributes<T>()
+            where T : Attribute
+        {
+            object[] customAttributes = mAssembly.GetCustomAttributes(typeof(T), false);
+
+            if ((null != customAttributes) && (customAttributes.Length > 0))
+                return ((T)customAttributes[0]);
+
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ NuSpeccer/Program.cs
@@ -0,0 +1,95 @@
+﻿#region License
+
+// --------------------------------------------------
+// Copyright © 2003–2010 OKB. All Rights Reserved.
+// 
+// This software is proprietary information of OKB.
+// USE IS SUBJECT TO LICENSE TERMS.
+// --------------------------------------------------
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace NuSpeccer
+{
+    internal class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            string filename = "";
+            if (args.Length == 0)
+            {
+                Stream myStream = null;
+                OpenFileDialog openFileDialog1 = new OpenFileDialog
+                {
+                    Filter = "dll files (*.dll)|*.dll|All files (*.*)|*.*",
+                    FilterIndex = 1,
+                    RestoreDirectory = true,
+                    Multiselect = false
+                };
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                    filename = openFileDialog1.FileName;
+                else
+                    return;
+            }
+            else
+                filename = Path.GetFullPath(args[0]);
+
+            if (File.Exists(filename))
+            {
+                Package package = new Package();
+                Assembly executingAssembly = Assembly.GetExecutingAssembly();
+
+                using (Stream stream = executingAssembly
+                    .GetManifestResourceStream(executingAssembly.GetName().Name + ".Package.nuspec"))
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        var xml = reader.ReadToEnd();
+                        package = Package.Deserialize(xml);
+                    }
+                }
+
+                Assembly assembly = Assembly.LoadFile(filename);
+                AssemblyInfoHelper helper = new AssemblyInfoHelper(assembly);
+                IDictionary<string, string> allAttributes = helper.GetAllAttributes();
+
+                allAttributes.Add("version", helper.FileVersion);
+                allAttributes.Add("id", helper.AssemblyName);
+                allAttributes.Add("authors", helper.Company);
+                allAttributes.Add("owners", helper.Company);
+
+                PropertyInfo[] properties = typeof(PackageMetadata).GetProperties(
+                    BindingFlags.Public |
+                    BindingFlags.DeclaredOnly |
+                    BindingFlags.Instance);
+
+                foreach (KeyValuePair<string, string> attribute in allAttributes)
+                {
+                    foreach (PropertyInfo propertyInfo in
+                        properties.Where(propertyInfo => propertyInfo.Name.ToLower().Equals(attribute.Key.ToLower())).
+                            Where(propertyInfo => propertyInfo.CanWrite))
+                        propertyInfo.SetValue(package.Metadata, attribute.Value, null);
+                }
+
+                var version = helper.RuntimeVersion;
+                package.Files.Add(new PackageFile
+                    {
+                        Src = helper.FilePath,
+                        Target = "lib"
+                    });
+
+                package.SaveToFile(filename.Replace(".dll", "") + ".nuspec");
+            }
+            else
+                Console.WriteLine("File does not exist!");
+        }
+    }
+}
